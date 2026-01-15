ba2098c7a8fd687d94b6f3a0c1014fd72b80b0b4
stbau04 <st.bauer04@gmail.com>
2026-01-15 19:52:45 +0100

    Add HyGames

---
+++ mods/HyGames/src/main/java/org/hymods/hygames/HyGamesPlugin.java
@@ -0,0 +1,23 @@
+package org.hymods.hygames;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+@SuppressWarnings("unused")
+public class HyGamesPlugin extends JavaPlugin {
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    @SuppressWarnings("unused")
+    public HyGamesPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+        LOGGER.atInfo().log("Setting up plugin " + this.getName());
+    }
+}

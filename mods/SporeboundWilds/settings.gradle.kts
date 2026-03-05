973213cb53c5f8e29ca91cbab5f46932899de545
Stephan Bauer <st.bauer04@gmail.com>
2026-03-05 20:07:58 +0100

    Rename ExamplePlugin

--- mods/SporeboundWilds/settings.gradle.kts
+++ mods/SporeboundWilds/settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "dev.hytalemodding"
+rootProject.name = "SporeboundWilds"
 
 plugins {
     // See documentation on https://scaffoldit.dev
@@ -21,8 +21,8 @@ hytale {
     }
 
     manifest {
-        Group = "HytaleModding"
-        Name = "ExamplePlugin"
-        Main = "dev.hytalemodding.ExamplePlugin"
+        Group = "HyMods"
+        Name = "Sporebound Wilds"
+        Main = "org.hymods.SporeboundWildsPlugin"
     }
 }
---
+++ mods/SporeboundWilds/src/main/java/org/hymods/SporeboundWildsPlugin.java
@@ -0,0 +1,17 @@
+package org.hymods;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class SporeboundWildsPlugin extends JavaPlugin {
+    public SporeboundWildsPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+
+    }
+}
